95ba61a4c9aa7cf3839f487ae8daa71f5e706d9a
pengcheng_XU <alexpengcheng@gmail.com>
2017-12-17 16:46:02 -0500

    Update ConfigWrapper.py

--- set_packing/wrapper/ConfigWrapper.py
+++ set_packing/wrapper/ConfigWrapper.py
@@ -6,16 +6,6 @@ class SetpackingWrapper(Wrapper):
     def __init__(self,env,para,embedding):
         env = env.unwrapped
 
-        # model parameters
-        # if 'total_vertices' in para: env.N = para.pop('total_vertices')
-        # if 'p' in para: env.p = para.pop("p")
-        # if 'theta' in para: env.expo = para.pop("theta")
-        # if 'mean' in para: env.mean = para.pop("mean")
-        # if 'std' in para: env.std = para.pop("std")
-        # if 'record' in para: env.record = para.pop("record")
-        # if 'path' in para: env.path = para.pop("path")
-        # env.trajectory = 0
-
         if 'data_path' in para: env.path = para.pop('data_path')
         if 'log_path' in para: env.log_path = para.pop('log_path')
         if 'N' in para: env.N = para.pop('N')
